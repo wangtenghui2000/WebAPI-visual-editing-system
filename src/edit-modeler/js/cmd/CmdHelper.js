de748fe1a14dd7afd6f2c09da40d63171d92495c
王腾辉 <88021249+wangtenghui2000@users.noreply.github.com>
2023-06-15 20:26:30 +0800

    Delete src directory

--- src/edit-modeler/js/cmd/CmdHelper.js
+++
@@ -1,9 +0,0 @@
-export default {
-    //创建更新元素属性的命令
-    createtUpdatePropertiesCmd(element,properties){
-        return {
-            cmd: 'element.updateProperties',
-            context: { element: element, properties: properties }
-        };
-    }
-};
